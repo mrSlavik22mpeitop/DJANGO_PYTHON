6e253ff5d1b3fdd42395f67f2712026fcc4efc47
вячеслав <vyacheslav@MacBook-Pro-vaceslav.local>
2020-12-15 16:52:41 +0300

    git

--- photoapp/payments/forms.py
+++ photoapp/payments/forms.py
@@ -8,11 +8,12 @@ class PaymentCreate(forms.ModelForm):
         fields = ('customers_id', 'products_id', 'payment_Date', 'amount')
         labels = {
             'customers_id': ('Имя покупателя'),
-            'products_id': ('Название товара'),
+            'products_id' : ('Название товара'),
             'payment_Date': ('Дата покупки'),
             'amount': ('Количество')
         }
 
         widgets = {
-            'payment_Date': forms.DateInput(attrs={'type': 'date'})
+            'payment_Date': forms.DateInput(attrs={'type': 'date'}),
+            'amount' : NumberInput(attrs={'maxlength': 50})
         }
